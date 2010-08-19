c256a1cff1859d8563d329116cb5dbefa25eb855
Alexis Sukrieh <sukria@sukria.net>
2010-08-19 15:32:29 +0200

    initial creation by module-starter

---
+++ lib/Test/TinyMocker.pm
@@ -0,0 +1,110 @@
+package Test::TinyMocker;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Test::TinyMocker - The great new Test::TinyMocker!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Test::TinyMocker;
+
+    my $foo = Test::TinyMocker->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Alexis Sukrieh, C<< <sukria at sukria.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-test-tinymocker at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-TinyMocker>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Test::TinyMocker
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Test-TinyMocker>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Test-TinyMocker>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Test-TinyMocker>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Test-TinyMocker/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Alexis Sukrieh.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Test::TinyMocker
